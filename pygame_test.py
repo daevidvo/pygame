a5394abb50aea2a721a1e889a0126a939521aeb5
daevidvo <37169924+daevidvo@users.noreply.github.com>
2022-11-21 02:05:22 -0800

    Add files via upload

--- pygame_test.py
+++ pygame_test.py
@@ -1,12 +1,27 @@
 import pygame
+from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode(
-    (800,400)
-)
+screen = pygame.display.set_mode((1600,900))
+pygame.display.set_caption('test title')
+clock = pygame.time.Clock()
 
+## creates a regular surface in the game.
+## below is the code for creating the surface along with the w,h of it.
+## test_surface = pygame.Surface((600,100))
+## below is the code for creating the fill of said surface along with the color.
+## test_surface.fill('Blue')
+
+test_surface = pygame.image.load('graphics/EeBQj8MUYAITUf6.jpg')
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            exit()
+    
+    screen.blit(test_surface,(0,0))
+
     pygame.display.update()
+    clock.tick(60)
+    
+
