2abf84fb93022d7912bb1346b284dda59d310f36
daevidvo <37169924+daevidvo@users.noreply.github.com>
2022-11-23 23:50:47 -0800

    Add files via upload

--- pygame_test.py
+++ pygame_test.py
@@ -13,9 +13,14 @@ test_font = pygame.font.Font(None,50)
 # below is the code for creating the fill of said surface along with the color.
 # test_surface.fill('Blue')
 
-sky_surface = pygame.image.load('graphics/EeBQj8MUYAITUf6.jpg')
-ground_surface = pygame.image.load('graphics/tumblr_oi0zvwAFAe1sfbymbo1_400.jpg')
-text_surafce = test_font.render('HELLO',True,'Green')
+#sky_surface = pygame.image.load('graphics/EeBQj8MUYAITUf6.jpg')
+#ground_surface = pygame.image.load('graphics/tumblr_oi0zvwAFAe1sfbymbo1_400.jpg')
+text_surface = test_font.render('HELLO',True,'black')
+sky_surface = pygame.Surface((1600,600))
+sky_surface.fill('light blue')
+ground_surface = pygame.Surface((1600,300))
+ground_surface.fill('dark green')
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -24,9 +29,9 @@ while True:
     
     # pygame draws in the order that you write the code. sky_surface will always be below the ground_surface
     screen.blit(sky_surface,(0,0))
-    screen.blit(ground_surface,(400,700))
+    screen.blit(ground_surface,(0,600))
     # the rendering of the text will always go over the top of the other layers since it's rendering as a display surface instead of a regular surface
-    screen.blit(text_surafce,(600,0))
+    screen.blit(text_surface,(700,0))
     pygame.display.update()
     clock.tick(120)
     
